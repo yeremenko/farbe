8ed7ace1b2f7a943783509c2993597ae1491ea38
Sergey Yeremenko <syeremenko@live.com>
2014-02-14 11:06:01 -0500

    added ability to successfully upload file

--- minions.js
+++ minions.js
@@ -1,5 +1,35 @@
 //learning JavaScript while creating a list of colors used on a web page
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script type="text/javascript">
-	alert('test');
-</script>
+$(document).ready(function(){
+
+
+	// Check for the various File API support.
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+	  // alert('Great success! All the File APIs are supported.');
+	} else {
+	  alert('The File APIs are not fully supported in this browser.');
+	}
+
+	//Event when user uploads file
+	$('#searchField').on('change' , function(){
+        var selectedFile = this.files[0];
+        alert('Successfully uploaded' + ' ' + selectedFile.name);
+        // alert('Successfully Uploaded' + ' ' + selectedFile.size);
+	});
+
+	//find colors in css files
+	function findColors() {
+
+		// $('p').on('click' , function() {
+		// 	var url = this.text();
+
+		// 	alert(url);
+		// });
+
+	};
+
+});
+
+
+
+
+//find inline colors
