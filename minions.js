23f8aa577d54ae6ceb8d89d362b24985f8962704
Sergey Yeremenko <syeremenko@live.com>
2014-03-04 12:09:16 -0500

    creates list with colors

--- minions.js
+++ minions.js
@@ -13,12 +13,12 @@ $(document).ready(function(){
 	//Event when user uploads file
 	$('#searchField').on('change' , function(){
         var selectedFile 		= this.files[0],
-        	selectedFileName	= selectedFile.name;							//store file name
+        	selectedFileName	= selectedFile.name;												//store file name
 
         	//make sure uploaded file is a css
         	function checkFileExt () {
-        		var splitFileName 	= selectedFileName.split('.');				//splits file name based on "."
-        		var selectedFileExt	= splitFileName[splitFileName.length-1];	//get last index of array
+        		var splitFileName 	= selectedFileName.split('.');									//splits file name based on "."
+        		var selectedFileExt	= splitFileName[splitFileName.length-1];						//get last index of array
 
 				//Check if uploaded file is a CSS file
 				if (selectedFileExt != "css") {
@@ -30,42 +30,37 @@ $(document).ready(function(){
        	 			//Function that gets contents from inside file
        	 			function getFileContents () {
 		        		var reader = new FileReader();						
+		        		var colorList = [];
+						var $colorListUl = $('#colorList');
+
 
 		        		reader.onload 	 = function(contents) {
-		        			var fileContents = contents.target.result;			//Stores file contents
+		        			var fileContents = contents.target.result;								//Stores file contents
 
-		        			var hexMatch 	 = /#[0-9a-f]{3,6}( *)/gi;			//RegEx searches file for starting #hex, ends with ;, 
-							var hexGroup 	 = fileContents.match(hexMatch);	//Runs file contents through Regex
+		        			var hexMatch 	 = /#[0-9a-f]{3,6}( *)/gi;								//RegEx searches file for starting #hex, ends with ;, 
+							var hexGroup 	 = fileContents.match(hexMatch);						//Runs file contents through Regex
 
-		        			var rgbMatch	 = /(rgb\([^)]*\))/gi;				//RegEx matches rgb
-							var rgbGroup 	 = fileContents.match(rgbMatch);	//Runs file contents through Regex
+		        			var rgbMatch	 = /(rgb\([^)]*\))/gi;									//RegEx matches rgb
+							var rgbGroup 	 = fileContents.match(rgbMatch);						//Runs file contents through Regex
 
-							var rgbaMatch	 = /(rgba\([^)]*\))/gi;				//RegEx matches rgb
-							var rgbaGroup 	 = fileContents.match(rgbaMatch);	//Runs file contents through Regex
+							var rgbaMatch	 = /(rgba\([^)]*\))/gi;									//RegEx matches rgb
+							var rgbaGroup 	 = fileContents.match(rgbaMatch);						//Runs file contents through Regex
 
-							//var wordMatch	 = / /gi;							//RegEx matches word
-							//var wordGroup 	 = fileContents.match(wordMatch);	//Runs file contents through Regex
+							//var wordMatch	 = / /gi;												//RegEx matches word
+							//var wordGroup  = fileContents.match(wordMatch);						//Runs file contents through Regex
 
-							var allMatches	 = hexGroup + rgbGroup + rgbaGroup;
+							colorList	= $.merge(hexGroup, $.merge(rgbGroup, rgbaGroup));
 
-							function colorWrap () {
-								$( allMatches ).each( function () {
+							$(colorList).each(function (i, color) {
+								$('<li>' + color + '</li>').appendTo($colorListUl);
+							});
 
-								});
-								// $( "#colorList" ).append( "<li>" +  + "</li>" );
-								console.log(allMatches);
-
-							};
-		        			colorWrap();
-
-
-							//console.log('HEX' , hexGroup);
-							//console.log('RGB' , rgbGroup);
-							//console.log('RGBA' , rgbaGroup);
-							//console.log('WORD' , wordGroup);
-							// console.log(allMatches);
+							// console.log('HEX' , hexGroup);
+							// console.log('RGB' , rgbGroup);
+							// console.log('RGBA' , rgbaGroup);
+							// console.log('WORD' , wordGroup);
 		        		};
-		        		reader.readAsText(selectedFile);						//Gets file contents into string
+		        		reader.readAsText(selectedFile);											//Gets file contents into string
 
 		        	};
 
