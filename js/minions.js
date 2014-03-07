501f49a1e5f201cee49d88257bbddc390688077a
Sergey Yeremenko <syeremenko@live.com>
2014-03-07 01:02:13 -0500

    code cleanup

--- js/minions.js
+++ js/minions.js
@@ -1,91 +1,77 @@
 //Learning JavaScript while creating a list of colors used on a web page
-var arrayUnique = function(a) {
-    return a.reduce(function(p, c) {
-        if (p.indexOf(c) < 0) p.push(c);
-        return p;
-    }, []);
-};
 
 var getFileContents = function (selectedFile) {
-	var reader = new FileReader();												//Empty array for colors
+  var reader = new FileReader();
 
+  reader.onload = function(contents) {
+  var fileContents = contents.target.result;
 
-	reader.onload 	 = function(contents) {
-		var fileContents = contents.target.result;								//Stores file contents
+  var hexMatch  = /#[0-9a-f]{3,6}( *)/gi;
+  var hexGroup  = fileContents.toUpperCase().match(hexMatch);
 
-		var hexMatch 	 = /#[0-9a-f]{3,6}( *)/gi;								//RegEx searches file for starting #hex, ends with ;, 
-		var hexGroup 	 = fileContents.toUpperCase().match(hexMatch);			//Runs file contents through Regex and uppercases
+  var rgbMatch  = /(rgb\([^)]*\))/gi;
+  var rgbGroup  = fileContents.toUpperCase().match(rgbMatch);
 
-		var rgbMatch	 = /(rgb\([^)]*\))/gi;									//RegEx matches rgb
-		var rgbGroup 	 = fileContents.toUpperCase().match(rgbMatch);			//Runs file contents through Regex and uppercases
+  var rgbaMatch = /(rgba\([^)]*\))/gi;
+  var rgbaGroup = fileContents.toUpperCase().match(rgbaMatch);
+  
+  var colorList	= hexGroup.concat(rgbGroup).concat(rgbaGroup);
+  var $colorListUl = $('#color-list');
 
-		var rgbaMatch	 = /(rgba\([^)]*\))/gi;									//RegEx matches rgb
-		var rgbaGroup 	 = fileContents.toUpperCase().match(rgbaMatch);			//Runs file contents through Regex and uppercases
-
-		//var wordMatch	 = / /gi;												//RegEx matches word
-		//var wordGroup  = fileContents.match(wordMatch);						//Runs file contents through Regex
-
-		var colorList	= arrayUnique(hexGroup.concat(rgbGroup).concat(rgbaGroup));			//Combine all matched groups into colorList array
-		var $colorListUl = $('#color-list');									//Div where colors will be added to jQuery selector
-
-
-		//add each matched color to #colorList UL
-		$(colorList).each(function (i, color) {	
-			var $li = $('<li>');
+    //add each matched color to #colorList UL
+    $(colorList).each(function (i, color) {	
+      var $li = $('<li>');
 			
-			$li.addClass( 'color-wrapper' );
-			$li.css( 'background-color' , color );
-			$li.html( color );
-			$colorListUl.append( $li );
-		});
+      $li.addClass( 'color-wrapper' );
+      $li.css( 'background-color' , color );
+      $li.html( color );
+      $colorListUl.append( $li );
+    });
 
-		console.log('HEX' , hexGroup);
-		console.log('RGB' , rgbGroup);
-		console.log('RGBA' , rgbaGroup);
-		// console.log('WORD' , wordGroup);
+    console.log('HEX' , hexGroup);
+    console.log('RGB' , rgbGroup);
+    console.log('RGBA' , rgbaGroup);
 
-	};
-	reader.readAsText(selectedFile);											//Gets file contents into string
-
+    };
+    reader.readAsText(selectedFile);
 };
 
+//Check if uploaded file is a CSS file
+var isFileCss = function(selectedFileName, selectedFile) {
+  var splitFileName   = selectedFileName.split('.');
+  var selectedFileExt	= splitFileName[splitFileName.length-1];
 
-var  isFileCss = function(selectedFileName, selectedFile) {
-	var splitFileName 	= selectedFileName.split('.');									//splits file name based on "."
-	var selectedFileExt	= splitFileName[splitFileName.length-1];						//get last index of array
-
-	//Check if uploaded file is a CSS file
-	if (selectedFileExt != "css") {
-		return false
-	} else {
-		return true
-	};
+  if (selectedFileExt != "css") {
+    return false;
+  } else {
+    return true;
+  }
 };
 
 
 $(document).ready(function(){
 
-	// Check for the various File API support.
-	if (window.File && window.FileReader && window.FileList && window.Blob) {
-	  // alert('Great success! All the File APIs are supported.');
-	} else {
-	  alert('The File APIs are not fully supported in this browser.');
-	}
+  // Check for the various File API support.
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    // alert('Great success! All the File APIs are supported.');
+  } else {
+    alert('The File APIs are not fully supported in this browser.');
+  }
 
-	//Event when user uploads file
-	$('#searchField').on('change' , function(){
-        var selectedFile 		= this.files[0],
-			selectedFileName	= selectedFile.name;												//store file name
+  //Event when user uploads file
+  $('#searchField').on('change' , function(){
+    var selectedFile = this.files[0],
+    selectedFileName = selectedFile.name;
 
-		console.log('File Name: ' + selectedFileName);
-		console.log('File Size: ' + selectedFile.size + ' bytes');
+    console.log('File Name: ' + selectedFileName);
+    console.log('File Size: ' + selectedFile.size + ' bytes');
 
 		//make sure uploaded file is a css
 		if (isFileCss(selectedFileName)){
 			getFileContents(selectedFile);
 		} else {
-			alert("Not css, try again!")
-		};
+			alert("Not css, try again!");
+		}
 
 	});
 
